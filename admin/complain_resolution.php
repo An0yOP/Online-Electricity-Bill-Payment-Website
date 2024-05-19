3e509ee546a4b30a5bf23b6834cffecc978f4bce
An0yOP <anoymaity1@gmail.com>
2024-05-19 09:42:43 +0530

    Update complain_resolution.php

--- admin/complain_resolution.php
+++ admin/complain_resolution.php
@@ -17,8 +17,8 @@ require 'PHPMailer/SMTP.php';
            $mail->isSMTP();                       //Send using SMTP
             $mail->Host       = 'smtp.gmail.com'; //Set the SMTP server to send through
             $mail->SMTPAuth   = true;  //Enable SMTP authentication
-            $mail->Username   = 'anoymaity1@gmail.com';  //SMTP username
-            $mail->Password   = 'zdgrbixnpxfiedqj'; //SMTP password
+            $mail->Username   = 'Example@xyz.com';  //SMTP username
+            $mail->Password   = 'password'; //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
             $mail->Port       = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
